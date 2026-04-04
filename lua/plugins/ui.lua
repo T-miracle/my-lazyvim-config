7e0c8c971ae23d3acf6af3e3caffec1603c1e73c
Tmiracle <1720570275@qq.com>
2026-04-05 04:43:50 +0800

    fix: 移除错误版本的neo-tree

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,19 +9,5 @@ return {
       opts.options.always_show_bufferline = true
       return opts
     end,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts)
-      opts.window = opts.window or {}
-      opts.window.position = "left"
-      opts.filesystem = opts.filesystem or {}
-      opts.filesystem.follow_current_file = {
-        enabled = false, -- 跟随当前文件（可选）
-        leave_dirs_open = true, -- 切换文件时目录保持展开（更“固定”）
-      }
-
-      return opts
-    end,
-  },
+  }
 }
